c9ed3442e7c053fafe3aecc5671a43232a721d87
Abusalah0 <109g83@gmail.com>
2025-02-02 00:27:44 +0300

    fuck me

--- src/builtin_functions/unset.c
+++ src/builtin_functions/unset.c
@@ -6,18 +6,18 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:42:37 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/01 23:47:21 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/02 00:27:19 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void    ft_unset(char *arg, t_shell *shell)
+void    ft_unset(char *arg, t_env **envp)// not fucking working
 {
     t_env *tmp;
     t_env *prev;
 
-    tmp = shell->env_list;
+    tmp = *envp;
     prev = NULL;
     while (tmp)
     {
@@ -26,7 +26,7 @@ void    ft_unset(char *arg, t_shell *shell)
             if (prev)
                 prev->next = tmp->next;
             else
-                shell->env_list = tmp->next;
+                *envp = tmp->next;
             free(tmp->name);
             free(tmp->value);
             free(tmp);
