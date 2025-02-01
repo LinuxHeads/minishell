fed99cde05f6942b448023986a4112ec8f4abb3f
Abusalah0 <109g83@gmail.com>
2025-02-01 23:56:45 +0300

    better cd.c

--- src/builtin_functions/cd.c
+++ src/builtin_functions/cd.c
@@ -5,74 +5,82 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/27 15:41:43 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/01 23:37:55 by abdsalah         ###   ########.fr       */
+/*   Created: 2025/02/01 23:51:27 by abdsalah          #+#    #+#             */
+/*   Updated: 2025/02/01 23:55:57 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "../../include/minishell.h"
 
 void ft_cd(char *args, t_env **envp)
 {
-  char *path;
-  char cwd[1024];
-  char *oldpwd;
+    char *path;
+    char cwd[1024];
+    char *oldpwd;
 
-  // If no arguments, use $HOME
-  printf("%s\n", args);
-  args = ft_strtrim(args, " ");
-  if (args == NULL || args[0] == '\0')
-  {
-    path = ft_getenv("HOME", *envp);
-    if (path == NULL)
+    // Check for too many arguments (pseudo-code)
+    // if (num_args > 1) { ... error ... }
+    if (args == NULL || args[0] == '\0')
     {
-      write(2, "cd: HOME not set\n", 18);
-      return;
+        path = ft_getenv("HOME", *envp);
+        if (!path)
+        {
+            write(2, "cd: HOME not set\n", 17);
+            return;
+        }
     }
-  }
-  // If argument is "-", use $OLDPWD
-  else if (ft_strcmp(args, "-") == 0)
-  {
-    path = ft_getenv("OLDPWD", *envp);
-    if (path == NULL)
+    // Handle "cd -" (use $OLDPWD)
+    else if (ft_strcmp(args, "-") == 0)
     {
-      write(2, "cd: OLDPWD not set\n", 19);
-      return;
+        path = ft_getenv("OLDPWD", *envp);
+        if (!path)
+        {
+            write(2, "cd: OLDPWD not set\n", 19);
+            return;
+        }
     }
-    printf("%s\n", path);  // Print the directory to change to
-  }
-  else
-  {
-    path = args;  // Use the provided argument
-  }
+    else
+    {
+        path = args; // Use the provided path
+    }
 
-  // Save the current directory to OLDPWD
-  if (!getcwd(cwd, sizeof(cwd)))
-  {
-    perror("getcwd");
-    return;
-  }
-  oldpwd = ft_strdup(cwd);  // Remember the current directory
-  if (!oldpwd)
-  {
-    perror("strdup");
+    // Save current directory to OLDPWD
+    if (!getcwd(cwd, sizeof(cwd)))
+    {
+        perror("getcwd");
+        return;
+    }
+    oldpwd = ft_strdup(cwd);
+    if (!oldpwd)
+    {
+        perror("ft_strdup");
+        return;
+    }
 
-    return;
-  }
-  // Attempt to change the directory
-  if (chdir(path) < 0)
-  {
-    perror("cd");
-    free(oldpwd);  // Free allocated memory for oldpwd
-    return;
-  }
+    // Change directory
+    if (chdir(path) != 0)
+    {
+        perror("cd");
+        free(oldpwd);
+        return;
+    }
 
-  // Update OLDPWD and PWD
-  ft_setenv("OLDPWD", oldpwd, envp);
-  if (getcwd(cwd, sizeof(cwd)))
-  {
-    ft_setenv("PWD", cwd, envp);
-  }
+    // Print new directory for "cd -"
+    if (ft_strcmp(args, "-") == 0)
+        printf("%s\n", path);
 
-  free(oldpwd);  // Free allocated memory for oldpwd    }
+    // Update OLDPWD and PWD
+    ft_setenv("OLDPWD", oldpwd, envp);
+    if (!getcwd(cwd, sizeof(cwd)))
+    {
+        // Fallback to path if getcwd fails
+        ft_setenv("PWD", path, envp);
+    }
+    else
+    {
+        ft_setenv("PWD", cwd, envp);
+    }
+
+    free(oldpwd);
 }
