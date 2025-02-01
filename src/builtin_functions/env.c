ccec556a102b7c105a5e066f316c8ea9d6126fa7
Abd Salah <59089096+Abusalah0@users.noreply.github.com>
2025-02-01 23:41:26 +0300

    Update env.c

--- src/builtin_functions/env.c
+++ src/builtin_functions/env.c
@@ -3,10 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdsalah <abdsalah@std.42amman.com>        +#+  +:+       +#+        */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:42:42 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/01/27 15:42:43 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/01 23:41:19 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/minishell.h"
+
+void    ft_env(t_shell *shell) {
+    print_envp(shell->env_list);
+    shell->exit_status = 0;
+}
