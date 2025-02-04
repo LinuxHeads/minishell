881e299ce5300c2f96c14c3838279d71d5d77670
Abusalah0 <109g83@gmail.com>
2025-02-04 04:25:20 +0300

    arg handling

--- src/builtin_functions/env.c
+++ src/builtin_functions/env.c
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdsalah <abdsalah@std.42amman.com>        +#+  +:+       +#+        */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:42:42 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/03 19:04:32 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/04 04:21:58 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void    ft_env(t_shell *shell) {
+int    ft_env(char **args, t_shell *shell)
+{
+    if (args[0])
+    {
+        ft_putstr_fd("env: ", 2);
+        ft_putstr_fd("too many args", 2);
+        ft_putstr_fd("\n", 2);
+        return (1);
+    }
     print_envp(shell->env_list);
-    shell->exit_status = 0;
+    // shell->exit_status = 0;
+    return (0);
 }
