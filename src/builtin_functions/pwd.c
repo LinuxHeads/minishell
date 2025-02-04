d2ac94dcf09e79c84aaf18ce36a47074c167213c
Abusalah0 <109g83@gmail.com>
2025-02-04 04:43:43 +0300

    add pwd.c

---
+++ src/builtin_functions/pwd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 03:24:37 by abdsalah          #+#    #+#             */
+/*   Updated: 2025/02/04 04:41:19 by abdsalah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int ft_pwd(char **args)
+{
+    char cwd[1024];
+
+    if (args[0])
+    {
+        ft_putstr_fd("pwd: too many arguments\n", 2);
+        return (1);
+    }
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        ft_putendl_fd(cwd, 1);
+        return (0);
+    }
+    else
+    {
+        perror("getcwd");
+        return (1);
+    }
+}
