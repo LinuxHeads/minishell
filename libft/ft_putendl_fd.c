faf393b92d26be05c1acb343f3a4b85f3ddb0344
Abusalah0 <109g83@gmail.com>
2025-02-04 04:28:26 +0300

    const arg type

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,13 +6,13 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 17:28:19 by abdsalah          #+#    #+#             */
-/*   Updated: 2024/08/31 13:20:47 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/04 04:27:14 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(const char *s, int fd)
 {
 	int	i;
 
