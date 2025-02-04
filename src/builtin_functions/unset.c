f059e120f2fd61de678c75537dd43e209216127d
Abusalah0 <109g83@gmail.com>
2025-02-04 04:29:14 +0300

    working now, takes multiple args, norminette

--- src/builtin_functions/unset.c
+++ src/builtin_functions/unset.c
@@ -6,31 +6,37 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:42:37 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/02 00:27:19 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/04 04:26:20 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void    ft_unset(char *arg, t_env **envp)// not fucking working
+int    ft_unset(char **args, t_env **envp)
 {
     t_env *tmp;
     t_env *prev;
+    int i;
 
-    tmp = *envp;
-    prev = NULL;
-    while (tmp)
+    i = -1;
+    while (args[++i])
     {
-        if (tmp->name && ft_strcmp(tmp->name, arg) == 0)
+        tmp = *envp;
+        prev = NULL;
+        while (tmp)
         {
-            if (prev)
-                prev->next = tmp->next;
-            else
-                *envp = tmp->next;
-            free(tmp->name);
-            free(tmp->value);
-            free(tmp);
-            return ;
+            if (ft_strcmp(args[i], tmp->name) == 0)
+            {
+                if (prev)
+                    prev->next = tmp->next;
+                else
+                    *envp = tmp->next;
+                free_envp_node(tmp);
+                break;
+            }
+            prev = tmp;
+            tmp = tmp->next;
         }
     }
+    return (0);
 }
