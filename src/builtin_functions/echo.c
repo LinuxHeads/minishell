d84905db26f127d6a57e8471decd979c22369a59
Abusalah0 <109g83@gmail.com>
2025-02-06 03:15:27 +0300

    include the minishell header file and use ft_strcmp

--- src/builtin_functions/echo.c
+++ src/builtin_functions/echo.c
@@ -3,22 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:40:45 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/06 00:05:23 by ahramada         ###   ########.fr       */
+/*   Updated: 2025/02/06 03:14:56 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include "../../include/minishell.h"
 
 int ft_echo(char **args)
 {
     int i = 1;
     int newline = 1;
 
-    while (args[i] && strcmp(args[i], "-n") == 0)
+    while (args[i] && ft_strcmp(args[i], "-n") == 0)
     {
         newline = 0;
         i++;
