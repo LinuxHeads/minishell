66b67a62ec25748e45cfa07423e92fe9addcd2f7
Abusalah0 <109g83@gmail.com>
2025-02-12 01:09:08 +0300

    norm, broke something

--- src/execution/find_cmd.c
+++ src/execution/find_cmd.c
@@ -6,75 +6,85 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 06:21:46 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/11 05:35:40 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/12 01:08:25 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static char *find_executable_in_paths(char **cmd, char **paths, char **full_path, int *i)
+static char	*find_executable_in_paths(char **cmd, char **paths,
+		char **full_path, int *i)
 {
-    while (paths[*i])
-    {
-        *full_path = malloc(ft_strlen(paths[*i]) + ft_strlen(*cmd) + 2);
-        if (!*full_path)
-            return (NULL);
-        sprintf(*full_path, "%s/%s", paths[*i], *cmd);
-        if (access(*full_path, X_OK) == 0)
-        {
-            free_str_array(paths);
-            return (*full_path);
-        }
-        free(*full_path);
-        (*i)++;
-    }
-    return (NULL);
+	while (paths[*i])
+	{
+		*full_path = malloc(ft_strlen(paths[*i]) + ft_strlen(*cmd) + 2);
+		if (!*full_path)
+			return (NULL);
+		sprintf(*full_path, "%s/%s", paths[*i], *cmd);
+		if (access(*full_path, X_OK) == 0)
+		{
+			free_str_array(paths);
+			return (*full_path);
+		}
+		free(*full_path);
+		(*i)++;
+	}
+	return (NULL);
 }
 
-char			*find_command_path(char *cmd, char **envp)
+char	*find_helper(char **envp, char *cmd)
 {
-    char    *path_var;
-    char    **paths;
-    char    *full_path;
-    int     i;
-    
-    if (ft_strchr(cmd, '/'))
-    {
-        path_var = ft_strdup(cmd);
-        if (!path_var)
-            return (NULL);
-        if (access(path_var, X_OK) == 0)
-            return (path_var);
-        free(path_var);
-        return (NULL);
-    }
-    if (access(cmd, X_OK) == 0)
-    {
-        path_var = ft_strdup(cmd);
-        if (!path_var)
-            return (NULL);
-        return (path_var);
-    }
+	char	*path_var;
+	char	**paths;
+	char	*full_path;
+	int		i;
 
-    i = -1;
-    while (envp[++i])
-    {
-        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
-        {
-            path_var = envp[i] + 5;
-            paths = ft_split(path_var, ':');
-            if (!paths)
-            {
-                return (NULL);                
-            }
-            i = 0;
-            full_path = find_executable_in_paths(&cmd, paths, &full_path, &i);
-            if (full_path)
-                return (full_path);
-            free_str_array(paths);
-            return (NULL);
-        }
-    }
-    return (NULL);
+	i = -1;
+	while (envp[++i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path_var = envp[i] + 5;
+			paths = ft_split(path_var, ':');
+			if (!paths)
+			{
+				return (NULL);
+			}
+			i = 0;
+			full_path = find_executable_in_paths(&cmd, paths, &full_path, &i);
+			if (full_path)
+				return (full_path);
+			free_str_array(paths);
+			return (NULL);
+		}
+	}
+	return (NULL);
 }
 
+char	*find_command_path(char *cmd, char **envp)
+{
+	char	*path_var;
+	char	*path;
+
+	if (ft_strchr(cmd, '/'))
+	{
+		path_var = ft_strdup(cmd);
+		if (!path_var)
+			return (NULL);
+		if (access(path_var, X_OK) == 0)
+			return (path_var);
+		free(path_var);
+		return (NULL);
+	}
+	if (access(cmd, X_OK) == 0)
+	{
+		path_var = ft_strdup(cmd);
+		if (!path_var)
+			return (NULL);
+		return (path_var);
+	}
+	path = find_helper(envp, cmd);
+	if (path)
+		return (path);
+	return (NULL);
+}
