27238cd380839d014e293d07f8e20e27ba4ba717
Ahmad Ramadan <ahramada@c2r3s6.42amman.com>
2025-02-16 12:19:48 +0300

    fixed exit  norme

--- src/builtin_functions/exit.c
+++ src/builtin_functions/exit.c
@@ -3,29 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:30:45 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/14 15:21:27 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/16 12:18:46 by ahramada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	cheker_for_number(char **args, t_shell *shell)
+{
+	if (ft_isnumber(args[0]))
+	{
+		ft_putstr_fd("exit: too many arguments\n", 2);
+		return (1);
+	}
+	else
+	{
+		ft_putstr_fd("exit: numeric argument required\n", 2);
+		ft_exit_handler(shell, NULL, NULL, 2);
+		return (0);
+	}
+}
+
 int	ft_exit(char **args, t_shell *shell)
 {
 	if (args[0] && args[1])
 	{
-		if (ft_isnumber(args[0]))
-		{
-			ft_putstr_fd("exit: too many arguments\n", 2);
+		if (cheker_for_number(args, shell))
 			return (1);
-		}
-		else
-		{
-			ft_putstr_fd("exit: numeric argument required\n", 2);
-			ft_exit_handler(shell, NULL, NULL, 2);
-		}
 	}
 	if (args[0])
 	{
