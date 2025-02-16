53a860889acda99cc14fd3ff05a7c33adaf2140f
Ahmad Ramadan <ahramada@c2r3s6.42amman.com>
2025-02-16 12:09:21 +0300

    fixed signals norme

--- src/signals.c
+++ src/signals.c
@@ -3,59 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 01:23:07 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/15 19:19:33 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/16 12:08:30 by ahramada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void reset_signals_heredoc(void)
+void	reset_signals_heredoc(void)
 {
-	struct sigaction sa;
-	
+	struct sigaction	sa;
+
 	ft_bzero(&sa, sizeof(struct sigaction));
 	sa.sa_handler = &close_heredoc;
 	sa.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa, NULL);
 }
 
-void handle_sigint(int sig)
+void	handle_sigint(int sig)
 {
-    
-    g_signal_flag = sig;
-    write(STDOUT_FILENO, "\n", 1);
-    close (0);
+	g_signal_flag = sig;
+	write(STDOUT_FILENO, "\n", 1);
+	close(0);
 }
 
-// Reset signals in child processes
-void reset_signals(void)
+void	reset_signals(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    ft_bzero(&sa, sizeof(sa));
-    sa.sa_handler = SIG_DFL;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_handler = SIG_DFL;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
 }
 
-// Setup signal handling for interactive mode
-void signals_setup(int mode)
+void	signals_setup(int mode)
 {
-    struct sigaction sa;
-    
-    ft_bzero(&sa, sizeof(sa));
-    if (!mode)
-        sa.sa_handler = handle_sigint;
-    else
-        sa.sa_handler = SIG_IGN;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sigaction(SIGINT, &sa, NULL);
-    sa.sa_handler = SIG_IGN;
-    sigaction(SIGQUIT, &sa, NULL);
+	struct sigaction	sa;
+
+	ft_bzero(&sa, sizeof(sa));
+	if (!mode)
+		sa.sa_handler = handle_sigint;
+	else
+		sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &sa, NULL);
 }
