c807e18b4ecd4c98f65fc2e85729f99d13248cbd
Ahmad Ramadan <ahramada@c2r6s3.42amman.com>
2025-02-18 16:02:32 +0300

    fixed norme git add * im sorry i dont remember which files i changed

--- src/expander/expand_env_helpers.c
+++ src/expander/expand_env_helpers.c
@@ -6,7 +6,7 @@
 /*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 13:31:36 by ahramada          #+#    #+#             */
-/*   Updated: 2025/02/17 16:12:31 by ahramada         ###   ########.fr       */
+/*   Updated: 2025/02/18 16:00:15 by ahramada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,18 @@ char	*process_var_value_spaces(char *var_value, int in_dq, char **result)
 	return (var_value);
 }
 
+int if_it_has_space(const char *str) {
+    if (!str) return 0; 
+
+    while (*str) {
+        if (*str == ' ')
+            return 1;
+        str++;
+    }
+    return 0;
+}
+
+
 int	setup_var_value(char **var_value, t_expander *ctx)
 {
 	*var_value = process_var_value_quotes(*var_value);
@@ -63,6 +75,10 @@ int	setup_var_value(char **var_value, t_expander *ctx)
 		return (-1);
 	}
 	*var_value = process_var_value_spaces(*var_value, ctx->in_dq, &ctx->result);
+	if (if_it_has_space(*var_value))
+	{
+    	*var_value = ft_strjoin(*var_value, " ");
+	}
 	if (!*var_value)
 		return (-1);
 	if (!*var_value[0])
