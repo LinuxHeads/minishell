10d773f412c1d79d0fc257188831c10f74a1221f
Abdelrahman Salah <abdsalah@c2r6s1.42amman.com>
2025-02-18 14:45:24 +0300

    fixed export behavior

--- src/builtin_functions/export/export.c
+++ src/builtin_functions/export/export.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:42:39 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/18 05:23:20 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:43:37 by ahramada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
+#include "../../../include/minishell.h"
 
 static void	print_export(t_env *env)
 {
@@ -39,10 +39,6 @@ static int	ft_printenv_sorted(t_env *env_list)
 	return (1);
 }
 
-/*
-** process_export_argument: validate syntax, split the variable, and update env.
-** Note: Now takes t_env **env_list so it can update the environment.
-*/
 static int	process_export_argument(const char *arg, t_env **env_list)
 {
 	char	*key;
@@ -50,7 +46,7 @@ static int	process_export_argument(const char *arg, t_env **env_list)
 	int		syntax;
 
 	syntax = export_syntax_error(arg);
-	if (!syntax || syntax == 2)
+	if (syntax != 1)
 	{
 		if (syntax == 2)
 			return (2);
@@ -62,6 +58,8 @@ static int	process_export_argument(const char *arg, t_env **env_list)
 	{
 		if (set_or_append_env(key, value, env_list))
 		{
+			free(key);
+			free(value);
 			return (1);
 		}
 	}
@@ -74,16 +72,20 @@ int	ft_export(char **args, t_env **env_list)
 {
 	int	i;
 	int	ret;
+	int	status;
 
 	i = 0;
+	ret = 0;
 	if (!args[0])
 		return (!ft_printenv_sorted(*env_list));
 	while (args[i])
 	{
-		ret = process_export_argument(args[i], env_list);
-		if (ret)
-			return (ret);
+		status = process_export_argument(args[i], env_list);
+		if (status == 2)
+			ret = 2;
+		else if (status == 1 && ret != 2)  // Only set ret to 1 if no option error occurred.
+			ret = 1;
 		i++;
 	}
-	return (0);
+	return (ret);
 }
--- src/builtin_functions/export/export_syntax.c
+++ src/builtin_functions/export/export_syntax.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_syntax.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 05:15:07 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/18 05:32:30 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:43:31 by ahramada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,26 @@ static int	validate_identifier_chars(const char *arg, size_t len)
 	return (1);
 }
 
+int	check_initial_syntax(const char *arg)
+{
+	if (ft_isdigit(arg[0]) || arg[0] == '=' || arg[0] == '\0')
+	{
+		ft_putstr_fd("minishell: export: `", STDERR_FILENO);
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+		return (0);
+	}
+	if (arg[0] == '-' || (arg[0] == '+' && ft_isalpha(arg[1])))
+	{
+		ft_putstr_fd("minishell: export: ", STDERR_FILENO);
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putstr_fd(": invalid option\n", STDERR_FILENO);
+		ft_putstr_fd("export: usage: export [-fn] [name[=value] ...] or export -p\n", STDERR_FILENO);
+		return (2);
+	}
+	return (1);
+}
+
 int	export_syntax_error(const char *arg)
 {
 	int			status;
@@ -51,24 +71,4 @@ int	export_syntax_error(const char *arg)
 	else
 		len = ft_strlen(arg);
 	return (validate_identifier_chars(arg, len));
-}
-
-int	check_initial_syntax(const char *arg)
-{
-	if (ft_isdigit(arg[0]) || arg[0] == '=' || arg[0] == '\0' || arg[0] == '+'
-		|| arg[0] == '-')
-	{
-		if (arg[0] == '-')
-		{
-			ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-			ft_putstr_fd(arg, STDERR_FILENO);
-			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-			return (2);
-		}
-		ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-		ft_putstr_fd(arg, STDERR_FILENO);
-		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-		return (0);
-	}
-	return (1);
 }
