9f62dd1c30da6aa026dfc3481f60f64e59d1022a
Abusalah0 <109g83@gmail.com>
2025-02-18 05:07:58 +0300

    add null checks and wrap logic

--- src/expander/cleanup_input.c
+++ src/expander/cleanup_input.c
@@ -6,36 +6,42 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 07:39:53 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/16 07:40:10 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/02/18 05:06:48 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static char	*replace_and_free(char *input, const char *old_sub, const char *new_sub)
+{
+	char	*result;
+
+	result = ft_str_replace(input, old_sub, new_sub);
+	free(input);
+	return (result);
+}
+
 char	*cleanup_input(char *input)
 {
-	char	*new_input;
-	char	*tmp;
+	char	*result;
 
 	if (!input)
 		return (NULL);
-	new_input = ft_str_replace(input, " > ", ">");
-	if (!new_input)
+	result = ft_str_replace(input, " > ", ">");
+	if (!result)
 		return (NULL);
-	tmp = new_input;
-	new_input = ft_str_replace(tmp, " < ", "<");
-	free(tmp);
-	tmp = new_input;
-	new_input = ft_str_replace(tmp, " >> ", ">>");
-	free(tmp);
-	tmp = new_input;
-	new_input = ft_str_replace(tmp, " << ", "<<");
-	free(tmp);
-	tmp = new_input;
-	new_input = ft_str_replace(tmp, " |", "|");
-	free(tmp);
-	tmp = new_input;
-	new_input = ft_str_replace(tmp, "| ", "|");
-	free(tmp);
-	return (new_input);
+	result = replace_and_free(result, " < ", "<");
+	if (!result)
+		return (NULL);
+	result = replace_and_free(result, " >> ", ">>");
+	if (!result)
+		return (NULL);
+	result = replace_and_free(result, " << ", "<<");
+	if (!result)
+		return (NULL);
+	result = replace_and_free(result, " |", "|");
+	if (!result)
+		return (NULL);
+	result = replace_and_free(result, "| ", "|");
+	return (result);
 }
